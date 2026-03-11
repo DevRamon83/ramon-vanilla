460956b64de947a34b2c72b912246eb76f232e01
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-03-11 11:20:02 +0100

    no comment

--- info/registry.js
+++ info/registry.js
@@ -1,20 +1,34 @@
+import {
+  explainAcceptOnly,
+  explainAtLeastOne,
+  explainIsObjValid,
+  explainTooLong,
+  explainTooShort,
+  explainTypeChecker,
+} from "./explainFuncs";
+
 export const registry = () => {
   const headerStyle =
     "font-weight: bold; color: #333; text-decoration: underline;";
   const funcStyle = "font-weight: bold; color: #000;";
-  const arrowStyle = "color: #999;";
   const descStyle = "color: #666; font-style: italic;";
 
   console.group("%cDATETIME", headerStyle);
   console.info(
-    "%ccapitalize() %c=> %creturns the string with the first letter capitalized",
+    "%ccapitalize() ➡️ %creturns the string with the first letter capitalized",
     funcStyle,
-    arrowStyle,
     descStyle,
   );
   console.groupEnd();
 
   console.group("%cVALIDATORS", headerStyle);
+
+  explainAcceptOnly();
+  explainAtLeastOne();
+  explainTooShort();
+  explainTooLong();
+  explainTypeChecker();
+  explainIsObjValid();
 
   console.groupEnd();
 
