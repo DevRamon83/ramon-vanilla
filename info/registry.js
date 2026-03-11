30c39d4667c5aec4dca7522f75a5098e55ab4888
DevRamon83 <172255607+DevRamon83@users.noreply.github.com>
2026-03-11 11:00:57 +0100

    no comment

--- info/registry.js
+++ info/registry.js
@@ -2,10 +2,15 @@ export const registry = () => {
   const headerStyle =
     "font-weight: bold; color: #333; text-decoration: underline;";
   const funcStyle = "font-weight: bold; color: #000;";
+  const arrowStyle = "color: #999;";
+  const descStyle = "color: #666; font-style: italic;";
 
   console.group("%cDATETIME", headerStyle);
   console.info(
     "%ccapitalize() %c=> %creturns the string with the first letter capitalized",
+    funcStyle,
+    arrowStyle,
+    descStyle,
   );
   console.groupEnd();
 
